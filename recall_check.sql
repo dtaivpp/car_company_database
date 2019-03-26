929bbf80cb1f7062cc357916c9d9247fd7d8892a
David Tippett <dtippett@Davids-MacBook-Pro-3.local>
2019-03-26 19:14:05 -0400

    Changed Jetrag to 7 speed

--- recall_check.sql
+++ recall_check.sql
@@ -8,4 +8,4 @@ FROM Customer_Ownership as CO
   LEFT OUTER JOIN Car_Vins as CV ON (CV.vin = CO.vin)
   LEFT OUTER JOIN Car_Options as COpt ON (COpt.option_set_id = CV.option_set_id)
   LEFT OUTER JOIN Car_Parts as CP on (CP.part_id = COpt.transmission_id)
-WHERE CP.part_recall = 1 and part_name='Jetrag 6 Speed';
+WHERE CP.part_recall = 1 and part_name='Jetrag 7 Speed';
